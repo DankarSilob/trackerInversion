000945ed02fbc1dadc4b6930ec0effc2d5cb8790
Daniel Sierra <dsierra.lob@gmail.com>
2013-02-19 09:28:26 -0800

    Creacion de proyecto

---
+++ Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using trackerInversion.Models;
+
+namespace trackerInversion.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user.IsValid(user.Usuario, user.Password))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                     ModelState.AddModelError("","Acceso Denegado");
+                }            
+            }
+
+            return View(user);
+        }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace trackerInversion.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage="El nombre de usuario es necesario")]
+        [Display(Name= "Usuario")]
+        public string Usuario {get; set;}
+
+        [Required(ErrorMessage = "La contraseña es necesaria")]
+        [DataType(DataType.Password)]
+        [Display(Name= "Contrasena")]
+        public string Password {get; set;}
+
+        public bool IsValid(string _usuario, string _password)
+        {
+            return true;
+        }
+    }
+
+
+}
